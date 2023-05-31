8a5e8f513d7e73abd2b4ac6785d51297c0692f91
veresoora <lahanabira@gmail.com>
2023-05-31 17:42:08 +0700

    memodifikasi class AbstraksiKlas dengan menghapus fungsi tampilan

--- Program abstraksi/Program abstraksi.cpp
+++ Program abstraksi/Program abstraksi.cpp
@@ -2,5 +2,14 @@
 using namespace std;
 
 class AbstraksiKlas {
+private: string x, y;
+public:
 
+	// Method untuk mengisi nilai
+	// Private member
+	void setXY(string a, string b) {
+		x = a;
+		x = b;
+	}
+	
 };
