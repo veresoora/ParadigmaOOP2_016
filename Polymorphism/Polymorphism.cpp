aaf47d47acb31d85a121133ca99ab66a49941530
veresoora <lahanabira@gmail.com>
2023-05-31 17:08:38 +0700

    penambahkan abstract class seseorang dengan virtual function pesan()

---
+++ Polymorphism/Polymorphism.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+class seseorang {
+public:
+	virtual void pesan() = 0;
+	// Virtual void pesan(){
+	// cout << "Pesan dari seseorang" << endl;
+	// }
+};
